c8dc46a9a0c66ccb3a75fd1e7cd2f2bcd63121e2
MONK <bm0905763@otc.edu>
2021-05-10 13:13:21 -0500

    fdsafd ss

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -2,7 +2,8 @@
   <div>
         <h1>Upcoming Movies</h1>
         <p v-for="movie in movies" :key=movie.id>
-            {{movie.id}}
+            {{movie.original_title}}
+            <img :src="`https://image.tmdb.org/t/p/w300/${movie.poster_path}`" alt="">
             <router-link :to="` /detail/${movie.id}`"><p>{{movie.id}}</p></router-link>
 
         </p>
@@ -22,6 +23,7 @@ export default {
     mounted()
     {
         axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=70a15c30fb26538a968ba499524a5e4e&language=en-US&page=1").then((response) =>{
+            console.log(response.data.results)
             this.movies = response.data.results;
         })
     }
