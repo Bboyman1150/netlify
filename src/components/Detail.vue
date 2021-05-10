5ee9a01882cf8999b0eb4ba9695503b960bfe6a6
MONK <bm0905763@otc.edu>
2021-05-10 12:56:20 -0500

    sifsof

---
+++ src/components/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1>Movie Detail</h1>
+    <p>{{detailObj.title}}</p>
+</div>
+  
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Detail',
+    data(){
+        return{
+            id:this.$route.params.id
+        },
+        mounted()
+        {
+            axios.get("https://api.themoviedb.org/3/movie/${movie_id}?api_key=70a15c30fb26538a968ba499524a5e4e&language=en-US").then((response) =>{
+                this.detailObj = response.data;
+                console.log(response.data)
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-      <h1>Upcoming Movies</h1>
+        <h1>Upcoming Movies</h1>
+        <p v-for="movie in movies" :key=movie.id>
+            {{movie.id}}
+            <router-link :to="` /detail/${movie.id}`"><p>{{movie.id}}</p></router-link>
 
+        </p>
  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name:'Movies'
+    name:'Movies',
+    data()
+    {
+        return{
+            movies:[]
+        }
+    },
+    mounted()
+    {
+        axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=70a15c30fb26538a968ba499524a5e4e&language=en-US&page=1").then((response) =>{
+            this.movies = response.data.results;
+        })
+    }
 
 }
 </script>
